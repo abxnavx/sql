1ed47af66a8d1194908ca8d40e712686e9c277ee
Abhinav <abhinavmanu0042@gmail.com>
2026-07-21 21:55:49 +0530

    Add SQL script for library database and books table

---
+++ library.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE library;
+CREATE TABLE books(id INT(25), title VARCHAR(30), author VARCHAR(30), price INT(30), genre VARCHAR(30));
+INSERT INTO books (id, title, author, price, genre) VALUES
+(1, 'The Great Gatsby', 'F. Scott Fitzgerald', 450, 'Fiction'),
+(2, 'The Alchemist', 'Paulo Coelho', 350, 'Fiction'),
+(3, 'A Brief History of Time', 'Stephen Hawking', 600, 'Science'),
+(4, 'The History of India', 'Romila Thapar', 500, 'History'),
+(5, 'Inferno', 'Dan Brown', 550, 'Thriller');
+SELECT * FROM `books` WHERE price>40;
